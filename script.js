6fc36d94a326c1a514b0f7b231ccfddd69edeace
yasusibando <yasusi.b@icloud.com>
2026-08-03 16:56:05 +0900

    create initial company website

---
+++ script.js
@@ -0,0 +1,16 @@
+const menuButton = document.querySelector('.menu-button');
+const siteNav = document.querySelector('.site-nav');
+
+if (menuButton && siteNav) {
+  menuButton.addEventListener('click', () => {
+    const isOpen = siteNav.classList.toggle('open');
+    menuButton.setAttribute('aria-expanded', String(isOpen));
+  });
+
+  siteNav.querySelectorAll('a').forEach((link) => {
+    link.addEventListener('click', () => {
+      siteNav.classList.remove('open');
+      menuButton.setAttribute('aria-expanded', 'false');
+    });
+  });
+}
